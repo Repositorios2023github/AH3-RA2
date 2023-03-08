5a788ab843491ec2cd24b8165c36d18b2d3870fe
Wilmer_Quexel <wilmer.estuardo.quexel@gmail.com>
2023-03-07 22:59:02 -0600

    first commit

---
+++ src/usuarios/Vendedores.java
@@ -0,0 +1,21 @@
+package usuarios;
+
+import java.awt.List;
+import java.util.LinkedList;
+import ventanas.CrearVendedor;
+import ventanas.Login;
+
+public class Vendedores {
+    
+    public static LinkedList<Vendedor> listaVendedores;
+
+    public static void main(String[] args) {
+
+      listaVendedores = new LinkedList<>();  
+      Login login = new Login();
+      login.setVisible(true);
+      
+      
+
+    }
+}
